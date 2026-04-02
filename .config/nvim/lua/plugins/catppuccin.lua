43974d1bc2f0c42bd79132b6d2eec5ea94ba7280
Roshan Studley <iluminated.horse@gmail.com>
2026-04-01 20:43:20 -0700

    update color stuff

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,7 +1,21 @@
 return {
-  'catppuccin/nvim',
-  priority = 1000,
-  config = function()
-    vim.cmd 'colorscheme catppuccin-macchiato'
-  end,
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      flavour = 'macchiato',
+      integrations = {
+        rainbow_delimiters = true,
+        treesitter = true,
+        noice = true,
+        snacks =true,
+      },
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd 'colorscheme catppuccin-macchiato'
+    end,
+  },
 }
+-- vim: ts=2 sts=2 sw=2 et
